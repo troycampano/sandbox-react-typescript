d0f4dd02bc04db0ec137afd1b5dbe012fc46235b
Troy Campano <troygeek@gmail.com>
2022-10-26 20:14:33 -0400

    Added component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type Props = {
+  name: string;
+}
+
+function Welcome(props: Props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
 function App() {
   return (
     <div className="App">
       Hello World
+      <Welcome name="Troy" />
     </div>
   );
 }
