0da6b4412061374d4794853a608367c2dba199e6
troycampano <troygeek@gmail.com>
2022-10-28 20:13:16 -0400

    Added links

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-    return (
-      <div>
-        <h1>This is the home page</h1>
-      </div>
-    );
-  }
-  
-  export default Home;
+  return (
+    <div>
+      <h1>This is the home page</h1>
+      <Link to="about">Click to view our about page</Link>
+      <br />
+      <Link to="contact">Click to view our contact page</Link>
+    </div>
+  );
+}
+
+export default Home;
